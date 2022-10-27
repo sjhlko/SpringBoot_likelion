35854778f3403afafe9e184b083924bc2dc50e1f
서정희 <sjhlko@naver.com>
2022-10-27 13:20:04 +0900

    get params 실습/request2(map 이용)

--- src/main/java/com/springboot/hello2/controller/HelloController.java
+++ src/main/java/com/springboot/hello2/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.springboot.hello2.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class HelloController {
@@ -26,6 +28,21 @@ public class HelloController {
     public String getVariable2(@RequestParam String name, @RequestParam String email,
                                @RequestParam String organization){
         return String.format("%s %s %s",name,email,organization);
+    }
+
+    @GetMapping(value = "/request2")
+    public String getVariable2(@RequestParam Map<String, String> param) {
+        param.entrySet().forEach((map)->{
+            System.out.printf("key:%s value:%s\n",map.getKey(),map.getValue());
+        });
+        return "request2가 호출 완료 되었습니다";
+    }
+
+
+    @GetMapping("/request3")
+    public String getVariable2(){
+        return null;
+        //return String.format("%s %s %s",name,email,organization);
     }
 
 }
