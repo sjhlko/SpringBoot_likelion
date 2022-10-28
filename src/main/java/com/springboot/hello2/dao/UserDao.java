c44e4cce1806f19f1f5e6b403d0ecc3b12cd3f3b
서정희 <sjhlko@naver.com>
2022-10-28 14:25:17 +0900

    UserDao 추가

---
+++ src/main/java/com/springboot/hello2/dao/UserDao.java
@@ -0,0 +1,53 @@
+package com.springboot.hello2.dao;
+
+import com.springboot.hello2.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDao {
+    private JdbcTemplate jdbcTemplate;
+    public  UserDao(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User(rs.getString("id"),
+                    rs.getString("name"),
+                    rs.getString("password"));
+            return user;
+        }
+    };
+
+    public void deleteAll(){
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount(){
+        return this.jdbcTemplate.queryForObject("select count(*) from users;", Integer.class);
+
+    }
+
+    public void add(final User user) {
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?);",
+                user.getId(),user.getName(),user.getPassword());
+    }
+
+    public User findById(String id) {
+        String sql = "select * from users where id = ?";
+        return this.jdbcTemplate.queryForObject(sql,rowMapper, id);
+
+    }
+    public List<User> getAll(){
+        String sql = "select * from users order by id";
+        return this.jdbcTemplate.query(sql,rowMapper);
+
+    }
+
+}
