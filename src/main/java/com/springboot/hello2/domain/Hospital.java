e0c99369243d15f90c4b24bd574b922ff93d9939
서정희 <sjhlko@naver.com>
2022-10-31 14:31:22 +0900

    Hospital domain 추가

---
+++ src/main/java/com/springboot/hello2/domain/Hospital.java
@@ -0,0 +1,32 @@
+package com.springboot.hello2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Hospital {
+    private int id;
+    private String openServiceName; //카멜 표기법
+    private int openLocalGovernmentCode;
+    private String managementNumber;
+    private LocalDateTime licenseDate;
+    private int businessStatus;
+    private int businessStatusCode;
+    private String phone;
+    private String fullAddress;
+    private String roadNameAddress;
+    private String hospitalName;
+    private String businessTypeName;
+    private int healthcareProviderCount;
+    private int patientRoomCount;
+    private int totalNumberOfBeds;
+    private float totalAreaSize;
+
+}
